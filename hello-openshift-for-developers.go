ae349fd19f1089bc2a98ffe1d6c7805e964e29e8
nickandh <52782780+nickandh@users.noreply.github.com>
2022-06-10 18:04:16 +0100

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,11 +9,11 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Developers!"
+		response = "Hello OpenShift!"
 	}
 
 	fmt.Fprintln(w, response)
-	fmt.Println("Servicing an impatient beginner's request.")
+	fmt.Println("Servicing request.")
 }
 
 func listenAndServe(port string) {
@@ -29,6 +29,12 @@ func main() {
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = "8080"
+	}
+	go listenAndServe(port)
+
+	port = os.Getenv("SECOND_PORT")
+	if len(port) == 0 {
+		port = "8888"
 	}
 	go listenAndServe(port)
 
